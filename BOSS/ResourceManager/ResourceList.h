44b923bd4fdac6cb8993eaa5c408dd226cc53790
Alexey Teplyakov <alexey.teplyakov91@gmail.com>
2013-05-25 22:02:47 +0400

    delete many old files

--- BOSS/ResourceManager/ResourceList.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RESOURCE_LIST_H_
-#define _RESOURCE_LIST_H_
-
-#include "Resources\Resource.h"
-#include "Resources\AnimationResource.h"
-#include "Resources\FontResource.h"
-#include "Resources\ImageResource.h"
-
-#endif
