735e231be3b0fd6cc57678edb74c125a04cfa91e
Alexey Teplyakov <alexey.teplyakov91@gmail.com>
2013-07-04 18:19:57 +0400

    added input #2

---
+++ engine/Input/Input.cpp
@@ -0,0 +1,57 @@
+#include <SDL.h>
+#include "Input.h"
+
+Input* Input::get_instance()
+{
+	static Input singleton;
+
+	static bool firstTime = true;
+	if (firstTime){
+		//getting the Lua class instance
+		singleton.lua = Lua::get_instance();
+		firstTime = false;
+	}
+
+	return &singleton;
+}
+
+bool Input::register_key(wchar_t key, std::string path)
+{
+	keyMap[key] = path;
+	return true;
+}
+
+//this function is used for luaponte binding
+bool bind_register_key(std::string key, std::string path)
+{
+	Input* instance = Input::get_instance();
+	instance->register_key(key[0], path);
+	return true;
+}
+
+bool Input::key_pressed(wchar_t key)
+{
+	return keyStates[key];
+}
+
+//this function is used for luaponte binding
+bool bind_key_pressed(std::string key)
+{
+	Input* instance = Input::get_instance();
+	return instance->key_pressed(key[0]);
+}
+
+bool Input::handle()
+{
+	SDL_PumpEvents();
+	keyStates = SDL_GetKeyState(nullptr);
+
+	//pressed keys handling
+	for(auto& it : keyMap){
+		if (keyStates[it.first] == true){
+			lua->do_file(it.second);
+		}
+	}
+
+	return true;
+}
---
+++ engine/Input/Input.h
@@ -0,0 +1,35 @@
+#ifndef _INPUT_H_
+#define _INPUT_H_
+
+#include <string>
+#include <map>
+#include <SDL.h>
+#include "../Lua/Lua.h"
+
+class Input{
+public:
+	//singleton
+	static Input* get_instance();
+
+	bool register_key(wchar_t key, std::string path);
+	bool key_pressed(wchar_t key);
+
+	bool handle();
+
+private:
+	std::map<wchar_t, std::string> keyMap;
+	Uint8* keyStates;
+	Lua* lua;
+
+	//hide it (singleton)
+	Input() = default;
+	~Input() = default;
+	Input(const Input&) = delete;
+	Input& operator=(const Input&) = delete;
+};
+
+//these functions are used for luaponte binding
+bool bind_register_key(std::string, std::string);
+bool bind_key_pressed(std::string);
+
+#endif
